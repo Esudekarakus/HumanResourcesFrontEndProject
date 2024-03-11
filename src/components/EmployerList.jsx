172eeb9e0b0560080dcf35a7c9d030c7cf300cac
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2024-03-11 12:03:19 +0300

    card

--- src/components/EmployerList.jsx
+++ src/components/EmployerList.jsx
@@ -1,10 +1,9 @@
-// src/components/EmployerList.jsx
-
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const EmployerList = () => {
   const [employers, setEmployers] = useState([]);
+  const defaultImageSrc = '/images/robert.png'; // Replace with the path to your default image
 
   useEffect(() => {
     const fetchEmployers = async () => {
@@ -29,9 +28,83 @@ const EmployerList = () => {
       <h2>İşveren Listesi</h2>
       <ul>
         {employers.map((employer) => (
-          <li key={employer.id}>
-            {employer.name} - {employer.lastName} - {employer.email} - {employer.phoneNumber} - {employer.address} - {employer.profession} - {employer.companyName}
-            <button onClick={() => handleDetailButtonClick(employer.id)}>Detayları Göster</button>
+          <li key={employer.id} className='card'>
+            <img
+              src={employer.imageSrc || defaultImageSrc}
+              className='card-img-top'
+              alt={`${employer.name}'s image`}
+            />
+            <div className='card-body'>
+              <div className='form-group'>
+                {/* Remove the file input as requested */}
+              </div>
+
+              <div className='form-group'>
+                <input
+                  className={'form-control-file'}
+                  placeholder='Employee Name'
+                  name='name'
+                  value={`${employer.name} ${employer.lastName}`}
+                  disabled
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  className={'form-control-file'}
+                  
+                  value={`${employer.email}`}
+                  disabled
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  className={'form-control-file'}
+                  
+                  value={`${employer.phoneNumber}`}
+                  disabled
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  className={'form-control-file'}
+                  
+                  value={`${employer.address}`}
+                  disabled
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  className={'form-control-file'}
+                  
+                  value={`${employer.department}`}
+                  disabled
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  className={'form-control-file'}
+                  
+                  value={`${employer.companyName}`}
+                  disabled
+                />
+              </div>
+              <div className='form-group'>
+                <input
+                  className='form-control'
+                  placeholder='Occupation'
+                  name='occupation'
+                  value={employer.profession}
+                  disabled
+                />
+              </div>
+              <div className='form-group text-center'>
+                <Link to={`/employer-details/${employer.id}`}>
+                  <button type='button' className='btn btn-light'>
+                    Detayları Göster
+                  </button>
+                </Link>
+              </div>
+            </div>
           </li>
         ))}
       </ul>

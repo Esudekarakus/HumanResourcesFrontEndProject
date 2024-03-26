171d7ee31bba34a63a10960e1c3b566d76309bc8
Esma Sude Karakuş <sudekarakus@outlook.com.tr>
2024-03-26 20:46:08 +0300

    update details resim yükleme eklendi

--- src/components/UpdateDetails.jsx
+++ src/components/UpdateDetails.jsx
@@ -1,41 +1,101 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import './UpdateDetails.css';
 import { useDispatch } from 'react-redux';
- 
+
+const defaultImageSrc='/images/defaultImage.jpg';
+const initialFieldValues ={
+  id:1,
+  phoneNumber:'',
+  address:'',
+  imageName:'',
+  imgSrc:defaultImageSrc,
+  imageFile:null
+}
 function PersonelGuncellemeFormu() {
-  const [personel, setPersonel] = useState({
-    fotograf: '',
-    telefon: '',
-    adres:'',// Yeni eklenen alan
-    // Diğer sabit bilgiler burada duruyor olacak, ancak bunları state'de tutmaya gerek yok.
-  });
+  const [values,setValues] =useState(initialFieldValues);
+  const [errors, setErrors] = useState({});
+ 
+
+
   const dispatch = useDispatch();
  
-  const handleChange = (e) => {
+  const handleChange = e => {
     const { name, value } = e.target;
-    setPersonel(prevState => ({
-      ...prevState,
+    setValues( {
+      ...values,
       [name]: value
-    }));
-  };
- 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // const response 
-    console.log(personel);
-  };
- 
+    })
+  }
+ const validate = () => {
+   let temp = {}
+   temp.imgSrc = values.imgSrc == defaultImageSrc?false : true;
+   temp.address= values.address == ""?false:true;
+   temp.phoneNumber = /^\d{11}$/.test(values.phoneNumber);
+   setErrors(temp)
+   return Object.values(temp).every(x=> x==true)
+ }
+ const handleSubmit = async (e) => {
+  e.preventDefault();
+  if (validate()) {
+    try {
+      const formData = new FormData();
+      formData.append('id', values.id);
+      formData.append('address', values.address);
+      formData.append('phoneNumber', values.phoneNumber);
+      formData.append('imageFile', values.imageFile); // imageFile'i FormData'ya ekleyin
+
+      const response = await fetch(`https://localhost:7287/api/Employee/1`, {
+        method: 'PUT',
+        body: formData
+      });
+
+      if (response.ok) {
+        console.log('Çalışan başarıyla güncellendi');
+        // Başarılı bir şekilde güncellendiğinde yapılacak işlemler buraya gelebilir
+      } else {
+        throw new Error('Çalışan güncellenirken bir hata oluştu');
+      }
+    } catch (error) {
+      console.error('Hata:', error.message);
+      // Hata durumunda yapılacak işlemler buraya gelebilir
+    }
+  }
+};
+
+  const showPreview = e =>{
+    if(e.target.files && e.target.files[0]){
+      let imageFile = e.target.files[0];
+      const reader =new FileReader();
+      reader.onload = x=>{
+        setValues({
+          ...values,
+          imageFile,
+          imgSrc : x.target.result
+        })
+      }
+      reader.readAsDataURL(imageFile)
+    }else{
+      setValues({
+        ...values,
+        imageFile:null,
+        imgSrc : defaultImageSrc
+      });
+    }
+  }
+
+  const applyErrorClass = field => ((field in errors && errors[field]==false)?'invalid-field':'')
   return (
 <div className="form-container">
 <form onSubmit={handleSubmit}>
+<img src={values.imgSrc}  className='card-img-top'  />
 <div  className="form-group">
-<label>Fotoğraf: <input type="file" name="fotograf" onChange={handleChange} /></label>
+<label>Fotoğraf: <input type="file" accept='image/*' /*sadece image format kabul etsin*/ name="fotograf" onChange={showPreview} className={""+applyErrorClass('imgSrc')}/></label>
 </div>
 <div className="form-group">
-<label>Adres: <input type="adres" name="adres" value={personel.adres} onChange={handleChange} /></label>
+<label>Adres: <input type="adres" name="address" value={values.address} onChange={handleChange} className={""+applyErrorClass('address')}/></label>
 </div>
 <div className="form-group" >
-<label>Telefon: <input type="tel" name="telefon" value={personel.telefon} onChange={handleChange} /></label>
+<label>Telefon: <input type="tel" name="phoneNumber" value={values.phoneNumber} onChange={handleChange}  className={""+applyErrorClass('phoneNumber')}/></label>
 </div>
 <button type="submit">Bilgileri Güncelle</button>
 </form>

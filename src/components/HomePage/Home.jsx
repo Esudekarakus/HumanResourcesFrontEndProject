30ea9aad7bacc33328821c89272b4b378570e385
gokcebulbul <146324263+gokcebulbul@users.noreply.github.com>
2024-03-27 15:58:23 +0300

    Sayfa Düzenlemeleri yapıldı

--- src/components/HomePage/Home.jsx
+++ src/components/HomePage/Home.jsx
@@ -1,72 +1,83 @@
-import Avatar from '@mui/material/Avatar';
-import CardContent from '@mui/material/CardContent';
-import Divider from '@mui/material/Divider';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-
-import React, { useState, useEffect } from 'react';
-import { fetchEmployerById } from '../../service/EmployerService';
-import { useParams } from 'react-router';
-
-
-
-import CircularProgress from '@mui/material/CircularProgress'; // 
-
-
-// import  { useState, useEffect } from 'react';
-// import { fetchEmployerById } from '../../service/EmployerService';
-// import { useParams } from 'react-router';
+import React from 'react';
 import { useSelector } from 'react-redux';
-
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import Divider from '@mui/material/Divider';
+import { List, ListItem, ListItemIcon, ListItemText } from '@mui/material';
+import EmailIcon from '@mui/icons-material/Email';
+import PhoneIcon from '@mui/icons-material/Phone';
+import HomeIcon from '@mui/icons-material/Home';
+import WorkIcon from '@mui/icons-material/Work';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box';
+ 
 function Home() {
-
   const userDetails = useSelector((state) => state.userDetails);
-
-
-
+ 
   if (!userDetails) {
-    return <CircularProgress />; 
+    return <CircularProgress />;
   }
-
+ 
   return (
-    <>
-    <div>
-      <h2 style={{ textAlign: 'center' }}>Anasayfa</h2>
-      <h5 style={{ textAlign: 'center' }}>Hoş geldiniz <strong>{userDetails.name}!</strong></h5>  
-      <br />
-      <Divider />
-      <br />
-      <CardContent>
-        <Stack spacing={2} sx={{ alignItems: 'center' }}>
-          <div>
-            <Avatar src={userDetails.avatar} sx={{ height: '120px', width: '120px' }} /> <br/>
-          </div>
-          <Stack spacing={1} sx={{ textAlign: 'left' }}>
-            <Typography variant="h6">
-              <label><strong>Ad Soyad:</strong></label> {userDetails.name}
-            </Typography>
-            <Typography color="" variant="h7">
-              <label><strong>Email:</strong></label> {userDetails.email}
-            </Typography>
-            <Typography color="" variant="h7">
-              <label><strong>Telefon:</strong></label> {userDetails.phoneNumber}
-            </Typography>
-            <Typography color="" variant="h7">
-              <label><strong>Adres:</strong></label> {userDetails.address}, <label></label>
-              {userDetails.address}
-            </Typography>
-            <Typography color="" variant="h7">
-              <label><strong>Departman:</strong></label> {userDetails.department}
-            </Typography>
-            <Typography color="" variant="h7">
-              <label><strong>İş Ünvanı:</strong></label> {userDetails.profession}
-            </Typography>
-          </Stack>
-        </Stack>
-      </CardContent>
-    </div>
-    </>
+<Box sx={{ flexGrow: 1, m: 2 }}>
+<Card raised sx={{ minWidth: 275 }}>
+<CardContent>
+<Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mb: 2 }}>
+<Avatar
+              src={userDetails.avatar}
+              sx={{ height: '120px', width: '120px', mb: 2 }}
+            />
+<Typography variant="h5" component="div" gutterBottom>
+              Hoş geldiniz {userDetails.name}!
+</Typography>
+</Box>
+<Divider variant="middle" sx={{ mb: 2 }} />
+<Paper elevation={0} sx={{ p: 2 }}>
+<List>
+<ListItem>
+<ListItemIcon>
+<WorkIcon />
+</ListItemIcon>
+<ListItemText primary="Ad Soyad" secondary={userDetails.name} />
+</ListItem>
+<ListItem>
+<ListItemIcon>
+<EmailIcon />
+</ListItemIcon>
+<ListItemText primary="Email" secondary={userDetails.email} />
+</ListItem>
+<ListItem>
+<ListItemIcon>
+<PhoneIcon />
+</ListItemIcon>
+<ListItemText primary="Telefon" secondary={userDetails.phoneNumber} />
+</ListItem>
+<ListItem>
+<ListItemIcon>
+<HomeIcon />
+</ListItemIcon>
+<ListItemText primary="Adres" secondary={userDetails.address} />
+</ListItem>
+<ListItem>
+<ListItemIcon>
+<WorkIcon />
+</ListItemIcon>
+<ListItemText primary="Departman" secondary={userDetails.department} />
+</ListItem>
+<ListItem>
+<ListItemIcon>
+<WorkIcon />
+</ListItemIcon>
+<ListItemText primary="İş Ünvanı" secondary={userDetails.profession} />
+</ListItem>
+</List>
+</Paper>
+</CardContent>
+</Card>
+</Box>
   );
 }
-
+ 
 export default Home;
